340d35a1a0f3647cc8bf9fd2cedff794c493c802
Alex Taranne <alextaranne@gmail.com>
2025-04-09 10:01:38 +0200

    add tabs-screen

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -14,14 +14,13 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<{
-  headerImage: ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
+
 }>;
 
 export default function ParallaxScrollView({
   children,
-  headerImage,
-  headerBackgroundColor,
+ 
+ 
 }: Props) {
   const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
@@ -54,10 +53,10 @@ export default function ParallaxScrollView({
         <Animated.View
           style={[
             styles.header,
-            { backgroundColor: headerBackgroundColor[colorScheme] },
+            ,
             headerAnimatedStyle,
           ]}>
-          {headerImage}
+        
         </Animated.View>
         <ThemedView style={styles.content}>{children}</ThemedView>
       </Animated.ScrollView>
